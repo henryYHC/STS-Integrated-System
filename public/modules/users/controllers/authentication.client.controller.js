4edf9bd515b0c297ada5abe248dca33ac3224fcd
Henry Chen <yuhsinchen19940823@gmail.com>
2015-06-22 01:02:48 -0400

    Updated angular user security

--- public/modules/users/controllers/authentication.client.controller.js
+++ public/modules/users/controllers/authentication.client.controller.js
@@ -22,7 +22,7 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 				$scope.authentication.user = response;
 
 				// And redirect to the index page
-				$location.path('/');
+				$location.path('/admin');
 			}).error(function(response) {
 				$scope.error = response.message;
 			});
