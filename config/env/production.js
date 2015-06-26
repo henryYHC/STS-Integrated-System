249203ca34ca32e381ed4cb032613f0d3e0ef59a
Henry Chen <yuhsinchen19940823@gmail.com>
2015-06-26 15:44:53 -0400

    Update produnction setting

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,8 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/sts-integratedsystem',
+	db: 'mongodb://localhost/sts-integratedsystem',
+    port: 80,
 	assets: {
 		lib: {
 			css: [
