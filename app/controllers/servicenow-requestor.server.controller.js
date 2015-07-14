f17b0954f7557b5e115ade4f434ba779f46dbd64
Henry Chen <yuhsinchen19940823@gmail.com>
2015-07-13 23:19:54 -0400

    Updated Service Now SOAP client controller

--- app/controllers/servicenow-requestor.server.controller.js
+++ app/controllers/servicenow-requestor.server.controller.js
@@ -5,13 +5,35 @@
  */
 var mongoose = require('mongoose'),
     _ = require('lodash'),
-    soap = require('soap');
+    request = require('request'),
+    soap = require('soap'),
+    fs = require('fs');
 
-var SN_WSDL = 'https://emorydev1.service-now.com/u_sts_incident.do?WSDL';
+// Get credentials (& reformat wsdl url)
+var credentialFilePath = __dirname + '/../credentials/ServiceNow.json',
+    credentialFile = fs.readFileSync(credentialFilePath, 'utf8'),
+    credential = JSON.parse(credentialFile);
+
+if(credential.username){
+    var index = credential.wsdl_url.indexOf('//') + '//'.length;
+    credential.wsdl_url = credential.wsdl_url.substring(0, index) + credential.username + ':' + credential.password + '@' + credential.wsdl_url.substring(index);
+}
 
 exports.createWalkinIncident = function(walkin){
-    soap.createClient(SN_WSDL, function(err, client){
-        if(err) console.log(err);
-        console.log(client);
+    var data = {
+        u_soapaction : 'CREATE',
+        u_record_type : 'Test Incident',
+        u_short_description : 'This is a test incident submitted by STS MEAN stack application',
+        u_assignment_group : 'LITS: Student Digital Life'
+    };
+
+    soap.createClient(credential.wsdl_url, function(err, client){
+        if(err) return console.log(err);
+        client.setSecurity(new soap.BasicAuthSecurity(credential.username, credential.password));
+
+        client.insert(data, function(err, response){
+            if(err) return console.log(err);
+            console.log(response);
+        });
     });
 };
