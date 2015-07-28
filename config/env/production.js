022273916c24d07d0afb4147e25da034040c63b1
Henry Chen <yuhsinchen19940823@gmail.com>
2015-07-28 13:30:51 -0400

    Update production config

--- config/env/production.js
+++ config/env/production.js
@@ -5,8 +5,10 @@ module.exports = {
 	assets: {
 		lib: {
 			css: [
-				'public/lib/bootstrap/dist/css/bootstrap.min.css',
-				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+                'public/lib/bootstrap/dist/css/bootstrap.css',
+                'public/lib/bootstrap-material-design/dist/css/roboto.min.css',
+                'public/lib/bootstrap-material-design/dist/css/material.min.css',
+                'public/lib/bootstrap-material-design/dist/css/ripples.min.css'
 			],
 			js: [
 				'public/lib/angular/angular.min.js',
@@ -17,7 +19,9 @@ module.exports = {
 				'public/lib/angular-sanitize/angular-sanitize.js', 
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+                'public/lib/bootstrap-material-design/dist/js/ripples.min.js',
+                'public/lib/bootstrap-material-design/dist/js/material.min.js'
 			]
 		},
 		css: 'public/dist/application.min.css',
