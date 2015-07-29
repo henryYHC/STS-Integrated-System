b284c9994bed307ead96240117145854b1f179f1
Henry Chen <yuhsinchen19940823@gmail.com>
2015-07-29 16:05:40 -0400

    switch to case-insensitive username

--- public/modules/users/controllers/authentication.client.controller.js
+++ public/modules/users/controllers/authentication.client.controller.js
@@ -14,6 +14,8 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
         };
 
 		$scope.signup = function() {
+            if($scope.credentials.username)
+                $scope.credentials.username = $scope.credentials.username.toLowerCase();
 			$http.post('/auth/signup', $scope.credentials).success(function(response) {
 				$location.path('/admin');
 			}).error(function(response) {
@@ -22,7 +24,8 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 		};
 
 		$scope.signin = function() {
-            if($scope.credentials.username) $scope.credentials.username.toLowerCase();
+            if($scope.credentials.username)
+                $scope.credentials.username = $scope.credentials.username.toLowerCase();
 			$http.post('/auth/signin', $scope.credentials).success(function(response) {
 				$scope.authentication.user = response;
 				$location.path('/admin');
