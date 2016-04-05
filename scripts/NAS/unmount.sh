773e785e986c2bd4565f69be3bba3c29ebca16fa
Henry Chen <yuhsinchen19940823@gmail.com>
2016-04-05 15:10:06 -0400

    Update

--- scripts/NAS/unmount.sh
+++ scripts/NAS/unmount.sh
@@ -2,5 +2,5 @@ DEST_ROOT='/mnt/nfs'
 DEST_DIR1='Db-Snapshots'
 DEST_DIR2='System-Info'
 
-sudo umount DEST_ROOT/$DEST_DIR1
-sudo umount DEST_ROOT/$DEST_DIR2
+sudo umount $DEST_ROOT/$DEST_DIR1
+sudo umount $DEST_ROOT/$DEST_DIR2
